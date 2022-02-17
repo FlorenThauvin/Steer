ed62d4934364233bfb49ff6230a685a55d296443
FlorenThauvin <florent_1999@hotmail.fr>
2022-02-17 19:13:12 +0100

    update circ 2

--- TP-Steer/Source/TP1_IA/VehiculeCircuit.cpp
+++ TP-Steer/Source/TP1_IA/VehiculeCircuit.cpp
@@ -22,19 +22,27 @@ FVector AVehiculeCircuit::CalculDirection() {
 	return SeekVelocity(Path);
 }
 
-void AVehiculeCircuit::TargCirc(){
+void AVehiculeCircuit::TargCirc() {
+	/*
+	The character follows a path continuously. The end of the path rejoins the
+	beginning, so that the character keeps following the same path indefinitely.
+	*/
 	FVector Path = ListTargets[Index]->GetActorLocation();
-	float Distance = (Path - GetActorLocation()).Size();
-	if (Distance <= DistanceChangeTarget) Index += Direction;
+	float Dist = (Path - GetActorLocation()).Size();
+	if (Dist <= DistanceChangeTarget) Index += Direction;
 	if (Index >= ListTargets.Num()) Index = 0;
 }
 
 void AVehiculeCircuit::TargOne(){
+	/*
+	The character follows a path that ends in a certain point. The character “arrives”
+	at that point and stops.
+	*/
 	if (IsArrived) return;
 	FVector Path = ListTargets[Index]->GetActorLocation();
-	float Distance = (Path - GetActorLocation()).Size();
-	if (Distance <= DistanceChangeTarget) Index += Direction;
-	if (Index >= ListTargets.Num()){
+	float Dist = (Path - GetActorLocation()).Size();
+	if (Dist <= DistanceChangeTarget) Index += Direction;
+	if (Index >= ListTargets.Num()) {
 		IsArrived = true;
 		Index = ListTargets.Num() - 1;
 	}
@@ -42,6 +50,12 @@ void AVehiculeCircuit::TargOne(){
 
 
 void AVehiculeCircuit::TargTwo(float DeltaTime){
+	/*
+	the character follows a path that ends in a certain point. The character
+	“arrives” at that point and then starts to follow the path on the opposite direction. When
+	the character “arrives” at the starting point, it starts to follow the path on the original
+	direction again.
+	*/
 	FVector Path = ListTargets[Index]->GetActorLocation();
 	float Distance = (Path - GetActorLocation()).Size();
 	if (IsArrived){
@@ -52,21 +66,14 @@ void AVehiculeCircuit::TargTwo(float DeltaTime){
 				Direction = -Direction;
 				IsArrived = false;
 				IsOnTarget = false;
-			}
-		}
-		return;
-	}
-
+			}}return;}
 	if (Distance <= DistanceChangeTarget) Index += Direction;
 	if (Index >= ListTargets.Num()){
 		IsArrived = true;
 		Index = ListTargets.Num() - 1;
 		CurrentTime = Time;
-	}
-	else if (Index <= 0 && Direction == -1){
-		IsArrived = true;
-		Index = 0;
-		CurrentTime = Time;
+		if (Index <= 0 && Direction == -1) Index = 0;
+		else Index = ListTargets.Num() - 1;
 	}
 }
 
