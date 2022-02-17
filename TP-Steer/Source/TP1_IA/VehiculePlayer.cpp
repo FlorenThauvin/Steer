909ec99618480624269dac86b4de06c57bda4629
FlorenThauvin <florent_1999@hotmail.fr>
2022-02-17 19:22:16 +0100

    update vehicule

--- TP-Steer/Source/TP1_IA/VehiculePlayer.cpp
+++ TP-Steer/Source/TP1_IA/VehiculePlayer.cpp
@@ -1,26 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "VehiculePlayer.h"
 #include "VehiculeIA.h"
-
 #include <Components/InputComponent.h>
 
-AVehiculePlayer::AVehiculePlayer()
-{
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
+AVehiculePlayer::AVehiculePlayer(){PrimaryActorTick.bCanEverTick = true;}
 
-// Called when the game starts or when spawned
-void AVehiculePlayer::BeginPlay()
-{
+void AVehiculePlayer::BeginPlay(){
 	Super::BeginPlay();
 	BindInput();
 }
 
-void AVehiculePlayer::BindInput()
-{
+void AVehiculePlayer::BindInput(){
 	InputComponent = NewObject<UInputComponent>(this);
 	InputComponent->RegisterComponent();
 	if (InputComponent)
@@ -33,40 +22,24 @@ void AVehiculePlayer::BindInput()
 	}
 }
 
-void AVehiculePlayer::Tick(float DeltaTime)
-{
-	
+void AVehiculePlayer::Tick(float DeltaTime){
 	FVector SteeringForce = Truncate(Direction, MaxForce);
 	FVector Acceleration = SteeringForce / Mass;
 	Velocity = Truncate(Velocity + Acceleration, MaxSpeed);
 	SetActorLocation(GetActorLocation() + Velocity);
-
 	SetActorRotation(FRotator(Velocity.Rotation()));
 }
 
-void AVehiculePlayer::Change()
-{
+void AVehiculePlayer::Change(){
 	AVehiculeIA* IA = Cast<AVehiculeIA>(Vehicule);
-	if (IA)
-	{
-		IA->ChangeAlgo();
-	}
+	if (IA) IA->ChangeAlgo();
 }
 
-void AVehiculePlayer::MoveForward(float Value)
-{
+void AVehiculePlayer::MoveForward(float Value){
 	Direction = FVector(Direction.X + Value, Direction.Y, Direction.Z);
-	/*
-	FVector Location = GetActorLocation();
-	SetActorLocation(FVector(Location.X + Value * MaxSpeed, Location.Y, Location.Z));
-	*/
 }
 
-void AVehiculePlayer::MoveRight(float Value)
-{
+void AVehiculePlayer::MoveRight(float Value){
 	Direction = FVector(Direction.X, Direction.Y + Value, Direction.Z);
-	/*
-	FVector Location = GetActorLocation();
-	SetActorLocation(FVector(Location.X, Location.Y + Value * MaxSpeed, Location.Z));
-	*/
+
 }
