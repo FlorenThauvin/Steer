0d5a855fb6510cc2a6df6675a83f4e29ea0dbb1b
FlorenThauvin <florent_1999@hotmail.fr>
2022-02-17 18:48:32 +0100

    update circ

--- TP-Steer/Source/TP1_IA/VehiculeCircuit.cpp
+++ TP-Steer/Source/TP1_IA/VehiculeCircuit.cpp
@@ -1,99 +1,73 @@
 #include "VehiculeCircuit.h"
 
-AVehiculeCircuit::AVehiculeCircuit()
-{
-	PrimaryActorTick.bCanEverTick = true;
-}
+AVehiculeCircuit::AVehiculeCircuit(){PrimaryActorTick.bCanEverTick = true;}
 
-void AVehiculeCircuit::BeginPlay()
-{
-	Super::BeginPlay();
-}
+void AVehiculeCircuit::BeginPlay(){Super::BeginPlay();}
 
-void AVehiculeCircuit::Tick(float DeltaTime)
-{
-	if (AlgoCircuit == AlgoCuircuit::CIRCUIT) ChangeTargetCircuit();
-	else if (AlgoCircuit == AlgoCuircuit::TWO_WAY) ChangeTargetTwo(DeltaTime);
-	else ChangeTargetOne();
-
+void AVehiculeCircuit::Tick(float DeltaTime){
+	if (AlgoCircuit == AlgoCuircuit::CIRCUIT) TargCirc();
+	else if (AlgoCircuit == AlgoCuircuit::ONE_WAY) TargOne();
+	else TargTwo(DeltaTime);
 	FVector SteeringDirection = CalculDirection();
-
 	FVector SteeringForce = Truncate(SteeringDirection, MaxForce);
 	FVector Acceleration = SteeringForce / Mass;
 	Velocity = Truncate(Velocity + Acceleration, MaxSpeed);
 	SetActorLocation(GetActorLocation() + Velocity);
-
 	SetActorRotation(FRotator(Velocity.Rotation()));
 }
 
-void AVehiculeCircuit::ChangeTargetCircuit()
-{
-	FVector TargetPath = ListTargets[IndexList]->GetActorLocation();
-	float Distance = (TargetPath - GetActorLocation()).Size();
-	if (Distance <= DistanceChangeTarget) IndexList += Direction;
-	if (IndexList >= ListTargets.Num()) IndexList = 0;
+FVector AVehiculeCircuit::CalculDirection() {
+	FVector Path = ListTargets[Index]->GetActorLocation();
+	if (IsArrived) return ArrivalVelocity(Path);
+	return SeekVelocity(Path);
 }
 
-void AVehiculeCircuit::ChangeTargetTwo(float DeltaTime)
-{
-	FVector TargetPath = ListTargets[IndexList]->GetActorLocation();
-	float Distance = (TargetPath - GetActorLocation()).Size();
+void AVehiculeCircuit::TargCirc(){
+	FVector Path = ListTargets[Index]->GetActorLocation();
+	float Distance = (Path - GetActorLocation()).Size();
+	if (Distance <= DistanceChangeTarget) Index += Direction;
+	if (Index >= ListTargets.Num()) Index = 0;
+}
 
-	if (IsArrival)
-	{
-		if (Distance <= 1.f) IsTarget = true;
+void AVehiculeCircuit::TargOne(){
+	if (IsArrived) return;
+	FVector Path = ListTargets[Index]->GetActorLocation();
+	float Distance = (Path - GetActorLocation()).Size();
+	if (Distance <= DistanceChangeTarget) Index += Direction;
+	if (Index >= ListTargets.Num()){
+		IsArrived = true;
+		Index = ListTargets.Num() - 1;
+	}
+}
 
-		if (IsTarget)
-		{
-			TimeWaitCurrent -= DeltaTime;
 
-			if (TimeWaitCurrent <= 0)
-			{
+void AVehiculeCircuit::TargTwo(float DeltaTime){
+	FVector Path = ListTargets[Index]->GetActorLocation();
+	float Distance = (Path - GetActorLocation()).Size();
+	if (IsArrived){
+		if (Distance <= 1.f) IsOnTarget = true;
+		if (IsOnTarget){
+			CurrentTime -= DeltaTime;
+			if (CurrentTime <= 0){
 				Direction = -Direction;
-				IsArrival = false;
-				IsTarget = false;
+				IsArrived = false;
+				IsOnTarget = false;
 			}
 		}
-		
 		return;
 	}
 
-	if (Distance <= DistanceChangeTarget) IndexList += Direction;
-	if (IndexList >= ListTargets.Num())
-	{
-		IsArrival = true;
-		IndexList = ListTargets.Num() - 1;
-		TimeWaitCurrent = TimeWait;
+	if (Distance <= DistanceChangeTarget) Index += Direction;
+	if (Index >= ListTargets.Num()){
+		IsArrived = true;
+		Index = ListTargets.Num() - 1;
+		CurrentTime = Time;
 	}
-	else if (IndexList <= 0 && Direction == -1)
-	{
-		IsArrival = true;
-		IndexList = 0;
-		TimeWaitCurrent = TimeWait;
+	else if (Index <= 0 && Direction == -1){
+		IsArrived = true;
+		Index = 0;
+		CurrentTime = Time;
 	}
 }
 
-void AVehiculeCircuit::ChangeTargetOne()
-{
-	if (IsArrival) return;
 
-	FVector TargetPath = ListTargets[IndexList]->GetActorLocation();
-	float Distance = (TargetPath - GetActorLocation()).Size();
-	
-	if (Distance <= DistanceChangeTarget) IndexList += Direction;
-	if (IndexList >= ListTargets.Num())
-	{
-		IsArrival = true;
-		IndexList = ListTargets.Num() - 1;
-	}
-}
-
-FVector AVehiculeCircuit::CalculDirection()
-{
-	FVector TargetPath = ListTargets[IndexList]->GetActorLocation();
-
-	if (IsArrival) return ArrivalVelocity(TargetPath);
-	return SeekVelocity(TargetPath);
-
-	//return ArrivalVelocity(Target);
-}
--- TP-Steer/Source/TP1_IA/VehiculeCircuit.h
+++ TP-Steer/Source/TP1_IA/VehiculeCircuit.h
@@ -1,7 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "VehiculeIA.h"
 #include "VehiculeCircuit.generated.h"
@@ -13,45 +10,35 @@ enum AlgoCuircuit
 	ONE_WAY	  UMETA(DisplayName = "One path"),
 	TWO_WAY   UMETA(DisplayName = "Two path"),
 };
-/**
- * 
- */
+
 UCLASS()
 class TP1_IA_API AVehiculeCircuit : public AVehiculeIA
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
 	AVehiculeCircuit();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	bool IsArrived = false;
+	bool IsOnTarget = false;
+	int Direction = 1;
+	int Index = 0;
+	float CurrentTime = 0;
 	UPROPERTY(EditAnywhere, Category = "Algorithme")
 	TEnumAsByte<AlgoCuircuit> AlgoCircuit;
-
 	UPROPERTY(EditAnywhere, Category = "Path")
 	TArray<AVehicule*> ListTargets;
-
 	UPROPERTY(EditAnywhere, Category = "Path")
 	float DistanceChangeTarget;
-
 	UPROPERTY(EditAnywhere, Category = "Path")
-	float TimeWait;
-
-	int Direction = 1;
-	int IndexList = 0;
-	bool IsArrival = false;
-	bool IsTarget = false;
-
-	float TimeWaitCurrent = 0;
+	float Time;
 
 public:
 	virtual void Tick(float DeltaTime) override;
-
 	FVector CalculDirection();
-	void ChangeTargetCircuit();
-	void ChangeTargetTwo(float DeltaTime);
-	void ChangeTargetOne();
+	void TargCirc();
+	void TargOne();
+	void TargTwo(float DeltaTime);
 };
