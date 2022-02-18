8d8b8970c27c0b51f41bc5d8e0af668ea80e1fc2
FlorenThauvin <florent_1999@hotmail.fr>
2022-02-18 19:30:28 +0100

    game mode

---
+++ TP-Steer/Source/TP1_IA/GM_Steer.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Steer.generated.h"
+
+
+UCLASS()
+class TP1_IA_API AGM_Steer : public AGameModeBase{GENERATED_BODY()};
