cfb76aaffb5d70828cf57d855e7fee90dfd36a43
FlorenThauvin <florent_1999@hotmail.fr>
2022-02-19 16:38:33 +0100

    correction crach circuit

--- TP-Steer/Source/TP1_IA/VehiculeCircuit.cpp
+++ TP-Steer/Source/TP1_IA/VehiculeCircuit.cpp
@@ -1,10 +1,10 @@
 #include "VehiculeCircuit.h"
 
-AVehiculeCircuit::AVehiculeCircuit(){PrimaryActorTick.bCanEverTick = true;}
+AVehiculeCircuit::AVehiculeCircuit() { PrimaryActorTick.bCanEverTick = true; }
 
-void AVehiculeCircuit::BeginPlay(){Super::BeginPlay();}
+void AVehiculeCircuit::BeginPlay() { Super::BeginPlay(); }
 
-void AVehiculeCircuit::Tick(float DeltaTime){
+void AVehiculeCircuit::Tick(float DeltaTime) {
 	if (AlgoCircuit == AlgoCuircuit::CIRCUIT) TargCirc();
 	else if (AlgoCircuit == AlgoCuircuit::ONE_WAY) TargOne();
 	else TargTwo(DeltaTime);
@@ -23,25 +23,17 @@ FVector AVehiculeCircuit::CalculDirection() {
 }
 
 void AVehiculeCircuit::TargCirc() {
-	/*
-	The character follows a path continuously. The end of the path rejoins the
-	beginning, so that the character keeps following the same path indefinitely.
-	*/
 	FVector Path = ListTargets[Index]->GetActorLocation();
-	float Dist = (Path - GetActorLocation()).Size();
-	if (Dist <= DistanceChangeTarget) Index += Direction;
+	float Distance = (Path - GetActorLocation()).Size();
+	if (Distance <= DistanceChangeTarget) Index += Direction;
 	if (Index >= ListTargets.Num()) Index = 0;
 }
 
-void AVehiculeCircuit::TargOne(){
-	/*
-	The character follows a path that ends in a certain point. The character “arrives”
-	at that point and stops.
-	*/
+void AVehiculeCircuit::TargOne() {
 	if (IsArrived) return;
 	FVector Path = ListTargets[Index]->GetActorLocation();
-	float Dist = (Path - GetActorLocation()).Size();
-	if (Dist <= DistanceChangeTarget) Index += Direction;
+	float Distance = (Path - GetActorLocation()).Size();
+	if (Distance <= DistanceChangeTarget) Index += Direction;
 	if (Index >= ListTargets.Num()) {
 		IsArrived = true;
 		Index = ListTargets.Num() - 1;
@@ -49,32 +41,32 @@ void AVehiculeCircuit::TargOne(){
 }
 
 
-void AVehiculeCircuit::TargTwo(float DeltaTime){
-	/*
-	the character follows a path that ends in a certain point. The character
-	“arrives” at that point and then starts to follow the path on the opposite direction. When
-	the character “arrives” at the starting point, it starts to follow the path on the original
-	direction again.
-	*/
+void AVehiculeCircuit::TargTwo(float DeltaTime) {
 	FVector Path = ListTargets[Index]->GetActorLocation();
 	float Distance = (Path - GetActorLocation()).Size();
-	if (IsArrived){
+	if (IsArrived) {
 		if (Distance <= 1.f) IsOnTarget = true;
-		if (IsOnTarget){
+		if (IsOnTarget) {
 			CurrentTime -= DeltaTime;
-			if (CurrentTime <= 0){
+			if (CurrentTime <= 0) {
 				Direction = -Direction;
 				IsArrived = false;
 				IsOnTarget = false;
-			}}return;}
+			}
+		}
+		return;
+	}
+
 	if (Distance <= DistanceChangeTarget) Index += Direction;
-	if (Index >= ListTargets.Num()){
+	if (Index >= ListTargets.Num()) {
 		IsArrived = true;
 		Index = ListTargets.Num() - 1;
 		CurrentTime = Time;
-		if (Index <= 0 && Direction == -1) Index = 0;
-		else Index = ListTargets.Num() - 1;
+	}
+	else if (Index <= 0 && Direction == -1) {
+		IsArrived = true;
+		Index = 0;
+		CurrentTime = Time;
 	}
 }
-
 
